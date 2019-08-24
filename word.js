7852a7b3b93962490ecb7b50bdc6574a07520d66
Michal Terranova <michalterranova@mail.com>
2019-08-23 20:51:40 -0400

    notes to working part

--- word.js
+++ word.js
@@ -1,4 +1,4 @@
-
+// this function goes through list of words and chooses one randomly from array
  function getWord(){
     let classical = ["paint the town red", "drink like a fish", "barking up the wrong tree", "when the rubber hits the road", "close but no cigar", "when the rubber hits the road", "everything but the kitchen sink", "keep your shirt on", "put a sock on it", "sight for sore eyes"];
     
