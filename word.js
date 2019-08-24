fbe399635c41a389c19bbf2e5d5382d90124157e
Michal Terranova <michalterranova@mail.com>
2019-08-23 20:53:03 -0400

    notes to working part

--- word.js
+++ word.js
@@ -1,9 +1,10 @@
 // this function goes through list of words and chooses one randomly from array
  function getWord(){
+     //array of phrases
     let classical = ["paint the town red", "drink like a fish", "barking up the wrong tree", "when the rubber hits the road", "close but no cigar", "when the rubber hits the road", "everything but the kitchen sink", "keep your shirt on", "put a sock on it", "sight for sore eyes"];
-    
+    // picking word
     chosenWord = classical[Math.floor(Math.random()*classical.length)];
-    
+    //returning word
     return chosenWord;
 }
 
